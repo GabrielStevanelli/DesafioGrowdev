3642998a1036e9ef82c507f4624d68cb23532fdf
Gabriel Stevanelli <Gabriel_Stevanelli@hotmail.com>
2022-05-15 20:27:50 -0300

    ajuste cors

--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,15 @@ import express from "express";
 import router from "./routes";
 import "reflect-metadata";
 import { AppDataSource } from "./data-source";
+import cors from "cors";
 
 const app = express();
 AppDataSource.initialize()
   .then(() => console.log("sucessfully database initialized"))
   .catch((error) => console.log(error));
+app.use(cors());
 app.use(express.json());
 app.use(router);
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
-    return res.status(200).json({});
-  }
-  next();
-});
-
 const port = process.env.PORT || 3000;
 
 app.listen(port);
